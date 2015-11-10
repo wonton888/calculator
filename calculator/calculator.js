3722fb7ca63b48235f62f553d8d35f4629f5680c
Adam <wonton916@gmail.com>
2015-11-09 16:56:23 -0800

    add initial files for calculator project

---
+++ calculator/calculator.js
@@ -0,0 +1,53 @@
+function run1(){
+
+  document.case.display.value += "1"
+};
+
+function run2(){
+  document.case.display.value += "2"
+};
+
+function run3(){
+  document.case.display.value += "3"
+};
+
+function run4(){
+  document.case.display.value += "4"
+};
+
+function run5(){
+  document.case.display += "5"
+};
+
+function run7(){
+  document.case.display.value += "7"
+};
+
+function run8(){
+  document.case.display.value += "8"
+};
+
+function run9(){
+  document.case.display.value += "9"
+};
+
+function runPlus(){
+  document.case.display.value += "+"
+};
+
+function runDivide(){
+  document.case.display.value += "/"
+};
+
+function runMultiply(){
+  document.case.display.value += "*"
+};
+
+function run(){
+  document.cse.display.value = ""
+};
+
+function runEquals() {
+  var equals = eval(document.case.display.value)
+  document.case.display.value = equals;
+}
