57aceda895b6a7e6395bf1b87a4a88d6ca0337d2
Adam <wonton916@gmail.com>
2015-11-10 11:50:01 -0800

    fix issues with js files, typos and syntax errors

--- js/calculator.js
+++ js/calculator.js
@@ -1,50 +1,76 @@
-function run1(){
+function run1() {
 
   document.case.display.value += "1"
 };
 
-function run2(){
+function run2() {
+
   document.case.display.value += "2"
 };
 
-function run3(){
+function run3() {
+
   document.case.display.value += "3"
 };
 
-function run4(){
+function run4() {
+
   document.case.display.value += "4"
 };
 
-function run5(){
-  document.case.display += "5"
+function run5() {
+
+  document.case.display.value += "5"
 };
 
-function run7(){
+function run6() {
+
+  document.case.display.value += "6"
+};
+
+function run7() {
+
   document.case.display.value += "7"
 };
 
-function run8(){
+function run8() {
+
   document.case.display.value += "8"
 };
 
-function run9(){
+function run9() {
+
   document.case.display.value += "9"
 };
 
-function runPlus(){
+function run0() {
+
+  document.case.display.value += "0"
+};
+
+function runPlus() {
+
   document.case.display.value += "+"
 };
 
-function runDivide(){
+function runMinus() {
+
+  document.case.display.value += "-"
+};
+
+function runDivide() {
+
   document.case.display.value += "/"
 };
 
-function runMultiply(){
+function runMultiply() {
+
   document.case.display.value += "*"
 };
 
-function run(){
-  document.cse.display.value = ""
+function runC() {
+
+  document.case.display.value = ""
 };
 
 function runEquals() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,31 +1,31 @@
-describe('Pizza', function(){
-  it("creates a pizza", function(){
-    var testPizza = new Pizza();
-    expect(testPizza).to.eql(new Pizza());
-});//not needed, but doesn't hurt.
-  it("it returns the quantity of pizza ordered", function(){
-    var testPizza = new Pizza(3, "pepperoni", "large");
-    expect(testPizza.quantity).to.equal(3);
-  });
-
-  it("it checks for the toppings that was inputed", function(){
-    var testPizza = new Pizza(3, "pepperoni", "large");
-    expect(testPizza.toppings).to.eql(["pepperoni"]);
-  });
-
-  it("it checks for the pizza size that was inputed", function(){
-    var testPizza = new Pizza(3, "pepperoni", "large");
-    expect(testPizza.size).to.equal("large");
-  });
-
-  it("finds the total cost of 3 large pizzas ordered with one topping", function(){
-    var testPizza = new Pizza(3, "pepperoni", "large");
-    expect(testPizza.totalPrice()).to.equal(66);
-  });
-
-  it("finds the total cost of 3 small pizzas ordered with one topping", function(){
-    var testPizza = new Pizza(3, "pepperoni", "small");
-    expect(testPizza.totalPrice()).to.equal(51);
-  });
-
-});
+// describe('Pizza', function(){
+//   it("creates a pizza", function(){
+//     var testPizza = new Pizza();
+//     expect(testPizza).to.eql(new Pizza());
+// });//not needed, but doesn't hurt.
+//   it("it returns the quantity of pizza ordered", function(){
+//     var testPizza = new Pizza(3, "pepperoni", "large");
+//     expect(testPizza.quantity).to.equal(3);
+//   });
+//
+//   it("it checks for the toppings that was inputed", function(){
+//     var testPizza = new Pizza(3, "pepperoni", "large");
+//     expect(testPizza.toppings).to.eql(["pepperoni"]);
+//   });
+//
+//   it("it checks for the pizza size that was inputed", function(){
+//     var testPizza = new Pizza(3, "pepperoni", "large");
+//     expect(testPizza.size).to.equal("large");
+//   });
+//
+//   it("finds the total cost of 3 large pizzas ordered with one topping", function(){
+//     var testPizza = new Pizza(3, "pepperoni", "large");
+//     expect(testPizza.totalPrice()).to.equal(66);
+//   });
+//
+//   it("finds the total cost of 3 small pizzas ordered with one topping", function(){
+//     var testPizza = new Pizza(3, "pepperoni", "small");
+//     expect(testPizza.totalPrice()).to.equal(51);
+//   });
+//
+// });
